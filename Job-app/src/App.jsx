ea4ac2fd71c20bc88e7981a9e9f98b412f75b479
LesegoLSG <217057620@student.uj.ac.za>
2024-05-04 20:47:32 -0500

    Added a route for singlejob

--- Job-app/src/App.jsx
+++ Job-app/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-import HomePage from './Pages/LandingPage/HomePage'
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 
 function App() {
 
 
   return (
     <>
-        <HomePage/>
+        <RouterProvider router={router}/>
     </>
   )
 }
