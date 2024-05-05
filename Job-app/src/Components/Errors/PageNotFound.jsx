4f444909b4e350eba6d7d33bf2dfe81d031f69dd
LesegoLSG <217057620@student.uj.ac.za>
2024-05-04 20:50:43 -0500

    Page not found error for routing

---
+++ Job-app/src/Components/Errors/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const PageNotFound = () => {
+  return (
+    <div className="flex justify-center items-center">
+        404 Not Found
+    </div>
+  )
+}
+
+export default PageNotFound
