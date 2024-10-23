d6776e054bc1fdda45994b973a89a02afd50b6d8
LesegoLSG <lesegomhlongo78@gmail.com>
2024-10-23 14:49:15 -0500

    Removed springboot backend

--- JobApplicationBackend/JobApplication/src/main/java/com/jobApply/JobApplication/Entities/Role.java
+++
@@ -1,6 +0,0 @@
-package com.jobApply.JobApplication.Entities;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- JobApplicationBackend/JobApplication/src/main/java/com/jobApply/JobApplication/Services/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.jobApply.JobApplication.Services;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService {
-    public UserDetailsService userDetailsService();
-}
